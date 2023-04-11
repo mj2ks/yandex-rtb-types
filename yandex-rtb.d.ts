9b00eb4b1adfa18d79d6750696555a7a633f6d44
den m <39779703+mj2ks@users.noreply.github.com>
2023-04-12 00:14:51 +0700

    Create yandex-rtb.d.ts

---
+++ yandex-rtb.d.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export namespace yandex {
+	interface yaContextCb {
+		push: (cb: CallableFunction) => void
+	}
+	namespace Ya {
+		namespace Context {
+			interface RenderData {
+				product: ['direct', 'rtb']
+			}
+			interface ErrorData {
+				code: number
+				text: string
+				type: ['error', 'warning']
+			}
+			interface RenderOptions {
+				async: boolean // ??
+				statId?: number
+				blockId: string
+				renderTo?: string
+				onRender?: (data: RenderData) => void
+				onError?: (data: ErrorData) => void
+				onClose?: () => void
+				videoCallbacks?: {}
+			}
+			class AdvManager {
+				static render: (options: RenderOptions, callback?: CallableFunction) => void
+				static destroy: () => void
+			}
+		}
+		
+	}
+	
+}
